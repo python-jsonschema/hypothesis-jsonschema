70a6b2b4e3a3abf90883a8fa67db81d81b0a830f
Zac Hatfield-Dodds <Zac-HD@users.noreply.github.com>
2018-11-21 17:22:16 +1100

    Allow dynamic typing for tests

--- test_hypothesis_jsonschema.py
+++ test_hypothesis_jsonschema.py
@@ -1,21 +1,14 @@
 """Tests for the hypothesis-jsonschema library."""
 
-from distutils.version import StrictVersion
 import os
 import subprocess
-from typing import List
-
-import hypothesis_jsonschema
 
 
-def test_version_is_semver() -> None:
-    """Check that the version string is semver-compliant."""
-    StrictVersion(hypothesis_jsonschema.__version__)
 
 
-def test_all_py_files_are_blackened() -> None:
+def test_all_py_files_are_blackened():
     """Check that all .py files are formatted with Black."""
-    files: List[str] = []
+    files = []
     for dirpath, _, fnames in os.walk("."):
         files.extend(os.path.join(dirpath, f) for f in fnames if f.endswith(".py"))
     assert len(files) >= 3
