bb9896f4a20d579735ce114fd6a2b89d8e56e7bc
Zac Hatfield-Dodds <Zac-HD@users.noreply.github.com>
2018-11-21 18:10:30 +1100

    Basic implementation
    This initial version does actually work... it's just intractable for all but the simplest schemata.  Weak but a functioning first draft.

--- README.md
+++ README.md
@@ -4,9 +4,12 @@ A [Hypothesis](https://hypothesis.readthedocs.io) strategy for generating data
 that matches some [JSON schema](https://json-schema.org/).
 It is currently in early alpha, but you can use it if you want.
 
-The public API consists of a single function, `hypothesis_jsonschema.from_schema`,
-which takes a JSON schema and returns a Hypothesis strategy which generates
-objects that match the schema.
+The public API consists of a just two functions:
+
+1. `hypothesis_jsonschema.from_schema` takes a JSON schema and returns a
+   Hypothesis strategy which generates objects that match the schema.
+2. `hypothesis_jsonschema.json_schemata` returns a Hypothesis strategy
+   which generates arbitrary JSON schemata.
 
 `hypothesis-jsonschema` does not support Python 2, because
 [it's close to end of life](https://pythonclock.org/) and Python 3.6+ is a
--- test_hypothesis_jsonschema.py
+++ test_hypothesis_jsonschema.py
@@ -3,7 +3,13 @@
 import os
 import subprocess
 
+from hypothesis import given, reject
+from hypothesis.errors import Unsatisfiable
+import hypothesis.strategies as st
+import jsonschema
+import pytest
 
+from hypothesis_jsonschema import from_schema, json_schemata
 
 
 def test_all_py_files_are_blackened():
@@ -18,3 +24,25 @@ def test_all_py_files_are_blackened():
         stdout=subprocess.PIPE,
         stderr=subprocess.PIPE,
     )
+
+
+@given(st.data(), json_schemata())
+def test_generated_data_matches_schema(data, schema):
+    """Check that an object drawn from an arbitrary schema is valid."""
+    try:
+        value = data.draw(from_schema(schema))
+    except Unsatisfiable:
+        reject()
+    jsonschema.validate(value, schema)
+
+
+def test_boolean_true_is_valid_schema_and_resolvable():
+    """...even though it's currently broken in jsonschema."""
+    from_schema(True).example()
+
+
+@pytest.mark.parametrize("schema", [None, False, {"type": "an unknown type"}])
+def test_invalid_schemas_raise(schema):
+    """Trigger all the validation exceptions for full coverage."""
+    with pytest.raises(Exception):
+        from_schema(schema).example()
