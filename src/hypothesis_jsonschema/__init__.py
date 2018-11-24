ea7dbbfde230a1b99a1ccc6b14fa40ef1ba6fded
Zac-HD <zac.hatfield.dodds@gmail.com>
2018-11-24 23:51:39 +1100

    Release version 0.2.0
    Almost puts the "V" in MVP.  Almost.

--- src/hypothesis_jsonschema/__init__.py
+++ src/hypothesis_jsonschema/__init__.py
@@ -1,6 +1,6 @@
 """A Hypothesis extension for JSON schemata."""
 
-__version__ = "0.1.0"
+__version__ = "0.2.0"
 __all__ = ["from_schema", "json_schemata"]
 
 from ._impl import from_schema, json_schemata
