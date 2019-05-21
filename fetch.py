6e49a8eabaefdec85f117d758bcf26af39491062
Zac-HD <zac.hatfield.dodds@gmail.com>
2019-05-21 12:53:50 +1000

    Update test corpus

--- fetch.py
+++ fetch.py
@@ -33,15 +33,19 @@ with concurrent.futures.ThreadPoolExecutor(max_workers=5) as ex:
         ex.submit(lambda c: (c["description"], get_json(c["url"])), c)
         for c in get_json("http://schemastore.org/api/json/catalog.json")["schemas"]
     ):
-        try:
-            name, value = future.result()
-        except Exception:
-            print(f"Could not retrieve schema: {name}")
+        for _ in range(3):
+            try:
+                name, value = future.result()
+            except Exception:
+                pass
+            else:
+                assert value is not None
+                # quick-and-dirty workaround for description collision with full schema.
+                if value.get("$id") != "http://json.schemastore.org/cryproj.dev.schema":
+                    schemata[name] = value
+                break
         else:
-            assert value is not None
-            # quick-and-dirty workaround for description collision with full schema.
-            if value.get("$id") != "http://json.schemastore.org/cryproj.dev.schema":
-                schemata[name] = value
+            print(f"Could not retrieve schema: {name}")
 
 # Dump them all back to the catalog file.
 with open("corpus-schemastore-catalog.json", mode="w") as f:
