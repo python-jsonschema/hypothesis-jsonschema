7ef02aefaaff85ff746cb4b8e775150dce0825cd
Zac-HD <zac.hatfield.dodds@gmail.com>
2019-08-06 19:37:38 +1000

    Use `tox` fr checks and testing

--- check.sh
+++
@@ -1,7 +0,0 @@
-autoflake --recursive --in-place --remove-all-unused-imports --remove-duplicate-keys --remove-unused-variables .
-pyupgrade --py36-plus $(find . -name '*.py')
-isort --recursive --apply .
-black .
-flake8
-mypy . src/
-pytest
