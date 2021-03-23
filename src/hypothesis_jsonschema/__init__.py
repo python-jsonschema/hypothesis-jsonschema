d6b98395ccdd34963f6b9a80fc19697a7226dec2
Zac-HD <zac.hatfield.dodds@gmail.com>
2021-03-23 13:18:06 +1100

    Release PyPy support (v0.19.1)

--- src/hypothesis_jsonschema/__init__.py
+++ src/hypothesis_jsonschema/__init__.py
@@ -3,7 +3,7 @@
 The only public API is `from_schema`; check the docstring for details.
 """
 
-__version__ = "0.19.0"
+__version__ = "0.19.1"
 __all__ = ["from_schema"]
 
 from ._from_schema import from_schema
